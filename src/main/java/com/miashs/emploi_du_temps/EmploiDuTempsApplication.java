32ad0cd954b033e3b6f8e63a4fb8ccd21925d3d8
walid.madad <walid.madad@etu.univ-tlse2.fr>
2024-10-15 15:48:42 +0200

    first commit

---
+++ src/main/java/com/miashs/emploi_du_temps/EmploiDuTempsApplication.java
@@ -0,0 +1,13 @@
+package com.miashs.emploi_du_temps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploiDuTempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploiDuTempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miashs/emploi_du_temps/EmploiDuTempsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miashs.emploi_du_temps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploiDuTempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
