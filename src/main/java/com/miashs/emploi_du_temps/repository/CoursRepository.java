3a855e9df6b0809795c90f6ef333a856d7bab030
walidmadad <walid.madad123@gmail.com>
2025-02-13 19:07:53 +0100

    update CoursRepository

--- src/main/java/com/miashs/emploi_du_temps/repository/CoursRepository.java
+++ src/main/java/com/miashs/emploi_du_temps/repository/CoursRepository.java
@@ -1,16 +1,13 @@
 package com.miashs.emploi_du_temps.repository;
 
 import com.miashs.emploi_du_temps.model.Cours;
-import com.miashs.emploi_du_temps.model.Departement;
+import com.miashs.emploi_du_temps.model.Enseignant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CoursRepository extends JpaRepository<Cours, Long> {
 
-    List<Cours> findByMatiere_Id (Long matiere_id);
-    List<Cours> findByEnseignant_Id (Long enseignant_id);
-    List<Cours> findByMatiere_IdAndTypedecours (Long matiere_id,String TypeDeCours);
-    List<Cours> findByEnseignant_IdAndTypedecours (Long enseignant_id,String TypeDeCours);
+    List<Cours> findByEnseignantId (Long enseignant_id);
 
 }
