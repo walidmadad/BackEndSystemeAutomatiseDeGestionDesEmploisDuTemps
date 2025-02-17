f6134013e1f15645c0fea3a7e98074412a9d6128
walidmadad <walid.madad123@gmail.com>
2025-02-17 22:29:57 +0100

    update WebConfig

--- src/main/java/com/miashs/emploi_du_temps/WebConfig.java
+++ src/main/java/com/miashs/emploi_du_temps/WebConfig.java
@@ -1,6 +1,10 @@
 package com.miashs.emploi_du_temps;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +17,18 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
+        http.authorizeHttpRequests(
+                auth ->  auth.requestMatchers("/api/v1/**").permitAll()
+                        .anyRequest().authenticated());
+        http.sessionManagement(session -> session.disable());
+        http.formLogin(login -> login.disable());
+        http.httpBasic(basic -> basic.disable());
+
+        return http.build();
     }
 }
