b62c31085770c9fe7fb8b152339558f41ac95525
walidmadad <walid.madad123@gmail.com>
2025-02-23 20:37:42 +0100

    update WebConfig

--- src/main/java/com/miashs/emploi_du_temps/WebConfig.java
+++ src/main/java/com/miashs/emploi_du_temps/WebConfig.java
@@ -3,6 +3,7 @@ package com.miashs.emploi_du_temps;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,12 +22,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(
                 auth ->  auth.requestMatchers("/api/v1/**").permitAll()
                         .anyRequest().authenticated());
-        http.sessionManagement(session -> session.disable());
-        http.formLogin(login -> login.disable());
+        http.sessionManagement(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(basic -> basic.disable());
 
         return http.build();
