54025cb57125652b72719dd64c98fb5da220d2f9
walidmadad <walid.madad123@gmail.com>
2025-03-05 12:00:00 +0100

    update Contrainte Repository

--- src/main/java/com/miashs/emploi_du_temps/repository/ContrainteRepository.java
+++ src/main/java/com/miashs/emploi_du_temps/repository/ContrainteRepository.java
@@ -1,7 +1,6 @@
 package com.miashs.emploi_du_temps.repository;
 
 import com.miashs.emploi_du_temps.model.Contrainte;
-import com.miashs.emploi_du_temps.model.Cours;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
