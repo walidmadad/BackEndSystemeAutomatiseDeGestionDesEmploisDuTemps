565ee1fc11b629ac5c6ad3cc3db01e6c9ab8ec79
walidmadad <walid.madad123@gmail.com>
2025-03-07 00:52:23 +0100

    update Cours Java

--- src/main/java/com/miashs/emploi_du_temps/model/Cours.java
+++ src/main/java/com/miashs/emploi_du_temps/model/Cours.java
@@ -17,7 +17,7 @@ import java.time.LocalTime;
 public class Cours {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String typeDeCours;
 
