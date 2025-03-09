c761ffab6763f5459f8341fd4870e52f484329fe
walidmadad <walid.madad123@gmail.com>
2025-03-09 15:43:07 +0100

    update Departement Entity

--- src/main/java/com/miashs/emploi_du_temps/model/Departement.java
+++ src/main/java/com/miashs/emploi_du_temps/model/Departement.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class Departement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String nom;
 
     @JsonIgnore
